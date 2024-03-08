629f453a2ee144c5b938b655c109f3f961098448
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-03-08 19:33:44 +0000

    html home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,20 +6,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	<div class="container">
+      <h1>Lista de Tareas</h1>
+      <input type="text" id="taskInput" placeholder="Agregar nueva tarea..." />
+      <ul id="taskList"></ul>
+    </div>
 	);
 };
 
