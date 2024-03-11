b20cf74c6bce631023517b3110a55d68a181398d
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-03-11 19:20:24 +0000

    proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-	<div class="container">
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTask = (event) => {
+    if (event.key === "Enter" && inputValue !== "") {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleTaskDelete = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="container">
       <h1>Lista de Tareas</h1>
-      <input type="text" id="taskInput" placeholder="Agregar nueva tarea..." />
-      <ul id="taskList"></ul>
+      <input
+        type="text"
+        id="taskInput"
+        placeholder="Agregar nueva tarea..."
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={addTask}
+      />
+      <ul id="taskList">
+        {tasks.length === 0 ? (
+          <li>No hay tareas, añadir tareas</li>
+        ) : (
+          tasks.map((task, index) => (
+            <li key={index} onMouseEnter={() => (document.getElementById(`task-${index}`).style.display = "block")} onMouseLeave={() => (document.getElementById(`task-${index}`).style.display = "none")}>
+              {task}
+              <i id={`task-${index}`} className="fas fa-trash-alt" style={{ display: "none" }} onClick={() => handleTaskDelete(index)}></i>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
-	);
+  );
 };
 
 export default Home;
+  
